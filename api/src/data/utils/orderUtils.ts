a66cd7df96ae3c30be35287873a832a9d95f8b09
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-11-24 21:45:38 +0800

    add order utils methods

--- api/src/data/utils/orderUtils.ts
+++ api/src/data/utils/orderUtils.ts
@@ -1,11 +1,24 @@
 import * as dayjs from 'dayjs';
-import { IOrder } from '../../db/models/definitions/orders';
+import { IOrder, IOrderDocument } from '../../db/models/definitions/orders';
 
 import { Orders } from '../../db/models/Orders';
 import { OrderItems } from '../../db/models/OrderItems';
 import { Products } from '../../db/models/Products';
 import { IPayment } from '../resolvers/mutations/orders';
 import { IOrderInput, IOrderItemInput } from '../types';
+import { BILL_TYPES } from '../../db/models/definitions/constants';
+import { IEbarimtConfig } from '../../db/models/definitions/configs';
+import { IOrderItemDocument } from '../../db/models/definitions/orderItems';
+import Customers from '../../db/models/Customers';
+import { sendRequest } from './commonUtils'
+import { DISTRICTS } from '../../db/models/definitions/constants';
+
+interface IDetailItem {
+  count: number;
+  amount: number;
+  inventoryCode: string;
+  productId: string;
+}
 
 export const generateOrderNumber = async (): Promise<string> => {
   const today = dayjs().hour(0).minute(0).second(0);
@@ -44,8 +57,11 @@ export const validateOrderPayment = (order: IOrder, doc: IPayment) => {
   }
 };
 
-export const updateOrderItems = async (orderId: string, items: IOrderItemInput[]) => {
-  const itemIds = items.map(item => item._id);
+export const updateOrderItems = async (
+  orderId: string,
+  items: IOrderItemInput[]
+) => {
+  const itemIds = items.map((item) => item._id);
 
   await OrderItems.deleteMany({ orderId, _id: { $nin: itemIds } });
 
@@ -58,7 +74,7 @@ export const updateOrderItems = async (orderId: string, items: IOrderItemInput[]
       const doc = {
         productId: item.productId,
         count: item.count,
-        orderId
+        orderId,
       };
 
       await OrderItems.createOrderItem(doc);
@@ -74,4 +90,100 @@ export const getTotalAmount = (items: IOrderItemInput[] = []): number => {
   }
 
   return total;
+};
+
+export const getDistrictName = (districtCode: string): string => {
+  if (DISTRICTS[districtCode]) {
+    return DISTRICTS[districtCode];
+  }
+
+  return ''
+};
+
+export const prepareEbarimtData = async (
+  order: IOrderDocument,
+  config: IEbarimtConfig,
+  items: IOrderItemDocument[] = [],
+  orderBillType: string,
+  registerNumber?: string
+) => {
+  if (!order) {
+    throw new Error('Order must be specified');
+  }
+
+  let billType = orderBillType || order.billType || BILL_TYPES.CITIZEN;
+  let customerCode = '';
+  let customerName = '';
+
+  if (registerNumber) {
+    const response = await sendRequest({
+      url: config.checkCompanyUrl,
+      method: 'GET',
+      params: { regno: registerNumber },
+    });
+
+    if (response.found) {
+      billType = '3';
+      customerCode = registerNumber;
+      customerName = response.name;
+    }
+  }
+
+  if (billType === BILL_TYPES.CITIZEN) {
+    const customer = await Customers.findOne({ _id: order.customerId });
+
+    if (customer && customer.code) {
+      customerCode = customer.code;
+    }
+  }
+
+  const productIds = items.map((item) => item.productId);
+  const products = await Products.find({ _id: { $in: productIds } });
+  const productsById = {};
+
+  for (const product of products) {
+    productsById[product._id] = product;
+  }
+
+  const details: IDetailItem[] = [];
+
+  for (const item of items) {
+    // if wrong productId then not sent
+    if (!productsById[item.productId]) {
+      continue;
+    }
+
+    const amount = (item.count || 0) * (item.unitPrice || 0);
+
+    details.push({
+      count: item.count,
+      amount,
+      inventoryCode: productsById[item.productId].code,
+      productId: item.productId,
+    });
+  }
+
+  const cashAmount = order.totalAmount || 0;
+
+  const orderInfo = {
+    date: new Date().toISOString().slice(0, 10),
+    orderId: order._id,
+    hasVat: config.hasVat || false,
+    hasCitytax: config.hasCitytax || false,
+    billType,
+    customerCode,
+    customerName,
+    description: order.number,
+    details,
+    cashAmount,
+    nonCashAmount: '0.00',
+    ebarimtResponse: {},
+  };
+
+  return {
+    ...orderInfo,
+    productsById,
+    contentType: 'pos',
+    contentId: order._id,
+  };
 };
