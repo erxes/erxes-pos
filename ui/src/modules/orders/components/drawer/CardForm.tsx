7b9f144813beb21cb71d6a60dc03500979b59986
Buyantogtokh <buyantogtokh.s@nmma.co>
2021-12-22 10:17:11 +0800

    update link

--- ui/src/modules/orders/components/drawer/CardForm.tsx
+++ ui/src/modules/orders/components/drawer/CardForm.tsx
@@ -7,7 +7,7 @@ import Button from "modules/common/components/Button";
 import Icon from "modules/common/components/Icon";
 import FormGroup from "modules/common/components/form/Group";
 import ControlLabel from "modules/common/components/form/Label";
-import { __ } from "modules/common/utils";
+import { __, Alert } from "modules/common/utils";
 import { Input } from "modules/orders/styles";
 import { PAYMENT_TYPES } from './CalculationForm';
 
@@ -41,6 +41,7 @@ export default class CardForm extends React.Component<Props, State> {
 
   render() {
     const { cardAmount, reset, color = '', onStateChange } = this.props;
+    const { checkedConnection } = this.state;
 
     const inputProps: any = {
       allowNegative: false,
@@ -59,9 +60,15 @@ export default class CardForm extends React.Component<Props, State> {
 
     // change databank urls below
     const onCheckConnection = () => {
-      fetch('http://localhost:7300/health').then(res => {
-        console.log(res, 'rerere');
-      })
+      const path = 'http://localhost:27028/ajax/get-status-info';
+
+      fetch(path).then((res: any) => {
+        if (res && res.status_code === 'ok') {
+          this.setState({ checkedConnection: true });
+        }
+      }).catch(e => {
+        Alert.error(`${e.message}: Databank-н төлбөрийн програмтай холбогдсонгүй: ${path}`);
+      });
     };
 
     return (
@@ -82,8 +89,8 @@ export default class CardForm extends React.Component<Props, State> {
           </Input>
         </FormGroup>
         <ButtonWrapper>
-          <Button btnStyle='simple' onClick={onCheckConnection}>{__("Send card amount")}</Button>
-          <Button btnStyle='warning'>{__("Check transaction")}</Button>
+          <Button btnStyle='simple' onClick={onCheckConnection}>{__("Check connection")}</Button>
+          {checkedConnection && <Button btnStyle='warning'>{__("Check transaction")}</Button>}
         </ButtonWrapper>
       </React.Fragment>
     );
