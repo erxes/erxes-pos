7da215616511b1a1f930436a26630d02b68b4fb3
anu-ujin <anu.b@nmma.co>
2021-12-22 19:13:38 +0800

    add kitchen screen ui

--- ui/src/modules/kitchen/components/Screen.tsx
+++ ui/src/modules/kitchen/components/Screen.tsx
@@ -5,14 +5,19 @@ import { IUser } from "modules/auth/types";
 import { PosWrapper, MainContent } from "../../orders/styles";
 import { IConfig } from "types";
 import { __ } from "modules/common/utils";
-import { IOrder } from "../../orders/types";
-import Table from 'modules/common/components/table';
+import { IOrder, FullOrderQueryResponse } from "../../orders/types";
+import Table from "modules/common/components/table";
+import { TableRow, Detail, Status, FlexEnd } from "../styles";
+import { colors } from "modules/common/styles";
+import Button from "modules/common/components/Button";
+import OrderSearch from "../containers/OrderSearch";
 
 type Props = {
   editOrder: (doc) => void;
   currentUser: IUser;
   currentConfig: IConfig;
   orders: IOrder[];
+  orderQuery: FullOrderQueryResponse;
 };
 
 type State = {};
@@ -26,74 +31,107 @@ export default class Screen extends React.Component<Props, State> {
 
   renderDetail(order) {
     const { items } = order;
+
     if (!items || !items.length) {
-      return <></>;
+      return null;
     }
 
-    return (
-      <>
-        {items.map(item => (
-          <p key={item._id}>
-            {item.productName} - ( {item.count} )
-          </p>
-        ))}
-      </>
-    )
+    return items.map((item) => (
+      <Detail key={item._id}>
+        <p>{item.productName}</p>
+        <span>
+          {__("Quantity")}: <b>{item.count}</b>
+        </span>
+      </Detail>
+    ));
   }
 
-  renderOrder(order: IOrder) {
-    const renderActions = () => {
-      return ''
+  renderStatus(primaryColor, status) {
+    let color = primaryColor;
+
+    switch (status) {
+      case "new":
+        color = colors.colorCoreBlue;
+        break;
+      case "doing":
+        color = colors.colorCoreOrange;
+        break;
     }
+
     return (
-      <tr
-        key={order._id}
-        id={order._id}
-      >
+      <Status color={color} odd={true}>
+        {__(status)}
+      </Status>
+    );
+  }
+
+  renderActions = (status) => {
+    if (status === "new") {
+      return (
+        <>
+          <Button size="small" btnStyle="primary" icon="play-1">
+            Start
+          </Button>
+          <Button size="small" btnStyle="danger" icon="cancel-1">
+            Decline
+          </Button>
+        </>
+      );
+    }
+
+    return (
+      <Button size="small" btnStyle="success" icon="check-circle">
+        Done
+      </Button>
+    );
+  };
+
+  renderOrder(order: IOrder) {
+    const { uiOptions } = this.props.currentConfig || ({} as IConfig);
+    const color = uiOptions.colors.primary;
+
+    return (
+      <TableRow key={order._id} id={order._id} color={color}>
+        <td className="number">{order.number.split("_")[1]}</td>
+        <td>{this.renderDetail(order)}</td>
         <td>
-          {order.number}
+          <Status color={color}>{__(order.type)}</Status>
         </td>
-        <td>
-          {this.renderDetail(order)}
-        </td>
-        <td>{order.type}</td>
-        <td>{order.status}</td>
-        <td>
-          {renderActions()}
-        </td>
-      </tr>
-    )
+        <td>{this.renderStatus(color, order.status)}</td>
+        <td>{this.renderActions(order.status)}</td>
+      </TableRow>
+    );
   }
 
   render() {
-    const { orders } = this.props;
-
+    const { orders, orderQuery } = this.props;
+    console.log(orders);
     return (
-      <PosWrapper>
-        <Row>
-          <Col>
-            <MainContent hasBackground={true}>
+      <MainContent hasBackground={true}>
+        <PosWrapper>
+          <FlexEnd>
+            <OrderSearch ordersQuery={orderQuery} />
+          </FlexEnd>
+          <Row>
+            <Col>
               <Table>
                 <thead>
                   <tr>
-                    <th>{__('Number')}</th>
-                    <th>{__('Order')}</th>
-                    <th>{__('Type')}</th>
-                    <th>{__('Status')}</th>
-                    <th >{__('Actions')}</th>
+                    <th>{__("Number")}</th>
+                    <th>{__("Order")}</th>
+                    <th>{__("Type")}</th>
+                    <th>{__("Status")}</th>
+                    <th>{__("Actions")}</th>
                   </tr>
                 </thead>
                 <tbody id="products">
-                  {orders.map(order => (
-                    this.renderOrder(order)
-                  ))}
+                  {orders.map((order) => this.renderOrder(order))}
                 </tbody>
               </Table>
-            </MainContent>
-
-          </Col>
-        </Row>
-      </PosWrapper>
+            </Col>
+          </Row>
+        </PosWrapper>
+      </MainContent>
     );
   } // end render()
 }
---
+++ ui/src/modules/kitchen/styles.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import styledTS from "styled-components-ts";
+import { dimensions, colors } from "modules/common/styles";
+import { rgba } from "modules/common/styles/ecolor";
+
+export const TableRow = styledTS<{ color?: string }>(styled.tr)`
+  background: ${(props) =>
+    props.color ? rgba(props.color, 0.08) : colors.colorShadowGray};
+
+  .number {
+    font-size: 20px;
+    font-weight: 600;
+  }
+`;
+
+export const Detail = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: ${dimensions.unitSpacing}px;
+  line-height: 15px;
+
+  p {
+    margin: 0;
+    font-weight: 500;
+    min-width: 120px;
+    max-width: 300px;
+    margin-right: ${dimensions.unitSpacing}px;
+  }
+`;
+
+export const Status = styledTS<{ color?: string; odd?: boolean }>(styled.span)`
+  background: ${(props) =>
+    props.odd
+      ? colors.colorWhite
+      : props.color
+      ? props.color
+      : colors.colorShadowGray};
+  color: ${(props) =>
+    props.odd
+      ? props.color
+        ? props.color
+        : colors.colorSecondary
+      : colors.colorWhite};
+  border: 1px solid ${(props) =>
+    props.odd
+      ? props.color
+        ? props.color
+        : colors.colorSecondary
+      : props.color};
+  padding: 6px 12px;
+  border-radius: 6px;
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 10px;
+`;
+
+export const FlexEnd = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: ${dimensions.coreSpacing}px;
+
+  > div {
+    width: 40%;
+  }
+`;
