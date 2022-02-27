b8a42522293d0eb7d948833ba49fb7943fcd0bb4
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-02-28 01:46:12 +0800

    visa card reader fake response

--- ui/src/modules/orders/components/splitPayment/cardPayment/CardInput.tsx
+++ ui/src/modules/orders/components/splitPayment/cardPayment/CardInput.tsx
@@ -83,43 +83,97 @@ export default class CardInput extends React.Component<Props, State> {
       this.setState({ amount: 0 });
     };
 
-    const PATH = 'http://localhost:27028';
+    // const PATH = 'http://localhost:27028';
+    const PATH = 'http://localhost:7000';
 
     const sendTransaction = async () => {
-      fetch(`${PATH}/ajax/get-status-info`).then(res => res.json()).then((res: any) => {
-        if (res && res.status_code === 'ok') {
-          // send transaction upon successful connection
-          fetch(PATH, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              service_name: 'doSaleTransaction',
-              service_params: {
-                // special character _ is not accepted
-                db_ref_no: number.replace('_', ''),
-                amount: amount.toString(),
-                vatps_bill_type: billType
-              }
+      fetch(`${PATH}/ajax/get-status-info`)
+        // .then(res => res.json())
+        .then((res: any) => {
+          // TODO remove code, fake data
+          res = {
+            status_code: 'ok',
+          }
+          if (res && res.status_code === 'ok') {
+            // send transaction upon successful connection
+            fetch(PATH, {
+              method: 'GET',
+              // method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              // body: JSON.stringify({
+              //   service_name: 'doSaleTransaction',
+              //   service_params: {
+              //     // special character _ is not accepted
+              //     db_ref_no: number.replace('_', ''),
+              //     amount: amount.toString(),
+              //     vatps_bill_type: billType
+              //   }
+              // })
             })
-          }).then(res => res.json()).then(r => {
-            if (r && r.status === true && r.response) {
-              if (r.response.response_code === '000') {
-                Alert.success(__(r.response.response_msg || 'Transaction was successful'));
+              // .then(res => res.json())
+              // .then(r => {
+              .then(res => {
+                // TODO remove code, fake data
+                const r = {
+                  status: true,
+                  response: {
+                    response_code: '000',
+                    aid: "A0000000031010",
+                    amount: amount,
+                    app_name: "VISA DEBIT",
+                    auth_code: "1TS93C",
+                    bank_mb_code: "05",
+                    batch_no: "000000000231",
+                    card_holder_name: "",
+                    date: "02/27",
+                    db_ref_no: "202202270001",
+                    entry_mode: "Contact Less TAP",
+                    is_vatps: "0",
+                    merchant_name: "Yoshinoya",
+                    model: "s300",
+                    operation: "SALE",
+                    pan: "438054XXXXXX2643",
+                    pos_firmware: "2.4.94",
+                    reader_id: "53240799",
+                    response_msg: "Гүйлгээ зөвшөөрөгдсөн.",
+                    rrn: "003841002333",
+                    tc: "0000000000000000",
+                    term_app_name: "DtbProlin",
+                    terminal_date: "20220227112935",
+                    terminal_id: "70078754",
+                    time: "11:29:32",
+                    trace_no: "020735",
+                    version: "334",
+                  }
+                }
+                console.log(number)
+                if (r && r.status === true && r.response) {
+                  if (r.response.response_code === '000') {
 
-                addCardPayment({ _id, cardInfo: JSON.stringify(r.response), amount });
-              } else {
-                return Alert.warning(r.response.response_msg);
-              }
-            }
-          }).catch(e => {
-            Alert.error(e.message);
-          })
-        }
-      }).catch(e => {
-        Alert.error(`${e.message}: Databank-н төлбөрийн програмтай холбогдсонгүй`);
-      });
+                    Alert.success(
+                      __(
+                        r.response.response_msg || 'Transaction was successful'
+                      )
+                    );
+
+                    addCardPayment({
+                      _id,
+                      cardInfo: JSON.stringify(r.response),
+                      amount
+                    });
+                  } else {
+                    return Alert.warning(r.response.response_msg);
+                  }
+                }
+              }).catch(e => {
+                Alert.error(e.message);
+              })
+          }
+        }).catch(e => {
+          Alert.error(`${e.message}: Databank-н төлбөрийн програмтай холбогдсонгүй`);
+        });
     };
 
     return (
