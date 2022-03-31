6d2726f00052c23ae9dd16de57005284ace3f9b2
Buyantogtokh <buyantogtokh.s@nmma.co>
2022-03-31 14:25:11 +0800

    change fake path

--- ui/src/modules/orders/components/splitPayment/cardPayment/CardInput.tsx
+++ ui/src/modules/orders/components/splitPayment/cardPayment/CardInput.tsx
@@ -71,7 +71,7 @@ export default class CardInput extends React.Component<Props, State> {
     };
 
     // const PATH = "http://localhost:27028";
-    const PATH = "http://localhost:7000";
+    const PATH = "https://test-pos.erxes.io/";
 
     const sendTransaction = async () => {
       // fetch(`${PATH}/ajax/get-status-info`)
