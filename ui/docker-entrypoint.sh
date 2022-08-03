12d3a6442b05b94540971e55c90db979f6e60865
munkhsaikhan.g@nmma.co <munkhsaikhan.g@nmma.co>
2022-08-04 02:19:41 +0800

    xos os ci 4

--- ui/docker-entrypoint.sh
+++ ui/docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 echo "window.env = `jo \`env | grep REACT_APP_\``" > /etc/nginx/html/js/env.js
 sed -i 's/${NGINX_HOST}/'"$NGINX_HOST"'/' /etc/nginx/conf.d/default.conf
-sed -i 's~%REACT_APP_API_URL%~'"$REACT_APP_API_URL"'~g' /etc/nginx/html/index.html
-sed -i 's~%REACT_APP_API_SUBSCRIPTION_URL%~'"$REACT_APP_API_SUBSCRIPTION_URL"'~g' /etc/nginx/html/index.html
 exec "$@"
