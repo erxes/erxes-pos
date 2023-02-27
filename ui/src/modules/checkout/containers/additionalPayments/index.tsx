8d548e8f390a7525c1d1e16fc3cadc43e4f1b704
KhBaterdene <baterdne.k@gmail.com>
2023-02-27 15:11:06 +0800

    little fix

--- ui/src/modules/checkout/containers/additionalPayments/index.tsx
+++ ui/src/modules/checkout/containers/additionalPayments/index.tsx
@@ -1,7 +1,7 @@
 import { useConfigsContext } from 'modules/auth/containers/Configs';
 import Payment from './Payment';
 import { KHANBANK_CARD } from '../KhanbankCard';
-import { GOLOMT_CARD } from '../golomtCard';
+import { GOLOMT_CARD } from '../golomtCard/useGolomt';
 import { TDB_CARD } from '../TDBCard/useTDB';
 
 const AdditionalPayments = () => {
